c824d3c7520599f5fde4c154ae866c6bfacfa313
Edoardo Biestro <167993193+EdoardoBie@users.noreply.github.com>
2026-03-01 18:30:50 +0100

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,9 @@ const Footer: React.FC = () => {
         <footer className="bg-[#0F0F0F] text-[#444] py-24 px-8 text-center font-mono text-xs border-t border-[#222]">
             <div className="max-w-md mx-auto space-y-4">
                 <p>CHEN JINGRONG (1917–1989)</p>
-                <p>NINE LEAVES SCHOOL</p>
+                <p>Archimede Treviglio (BG)</p>
                 <div className="w-8 h-[1px] bg-[#333] mx-auto my-8"></div>
-                <p>A digital tribute to the resilience of poetry in the face of history.</p>
+                <p>Biestro Edoardo, Dourka Rayan, Forlani Giorgio, Khabir Anouar, Jendoubi Francesco</p>
                 <p className="pt-12 opacity-50">SCROLL TO TOP TO REASSEMBLE</p>
             </div>
         </footer>
